38af205d7e4fdd3d1c80f94393b07f8db18f22ca
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2024-10-10 20:26:53 -0300

    update retorno.js

--- obc-html5/JAVASCRIPT-MODULO-1/aula-43/retorno.js
+++ obc-html5/JAVASCRIPT-MODULO-1/aula-43/retorno.js
@@ -1,5 +1,51 @@
 function calcularMedia(a, b) {
 	const media = (a + b) / 2;
+	return media;
 }
 
-calcularMedia();
+const resultado = calcularMedia(7, 2);
+
+console.log(resultado);
+
+function criarProduto(nome, preco) {
+	const produto = {
+		nome,
+		preco,
+		estoque: 1,
+	};
+	return produto;
+}
+
+console.log(criarProduto("Notebook Intel Core i3 8GB", 2500));
+
+function areaRetangular(base, altura) {
+	const area = base * altura;
+	return area;
+}
+
+function areaQuadrada(lado) {
+	return areaRetangular(lado, lado);
+}
+
+// console.log(areaRetangular(3, 5));
+
+// console.log(areaQuadrada(9));
+
+function ola() {
+	let texto = "...";
+	return texto;
+	texto = "Olá, Mundo";
+	console.log(texto);
+}
+
+console.log(ola());
+
+function maiorIdade(idade) {
+	if (idade >= 18) {
+		return "Maior de idade";
+	} else {
+		return "Menor de idade";
+	}
+}
+
+console.log(maiorIdade(12));
