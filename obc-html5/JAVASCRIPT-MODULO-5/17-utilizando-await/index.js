4babbb71c2f7ea5ebbe79a4b87138dda8afac6dd
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2024-11-21 21:34:06 -0300

    async-await

--- obc-html5/JAVASCRIPT-MODULO-5/17-utilizando-await/index.js
+++ obc-html5/JAVASCRIPT-MODULO-5/17-utilizando-await/index.js
@@ -4,3 +4,20 @@ async function asyncSum(a, b) {
 	}
 	return a + b;
 }
+
+// async function execute() {
+// 	asyncSum(50, 33).then((result) => {
+// 		console.log(result);
+// 	});
+// }
+
+async function execute() {
+	try {
+		const result = await asyncSum(50, null);
+		console.log(result);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+execute();
