05a1017abc59e9820bda389af2a9d7787654b175
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2024-12-06 23:48:53 -0300

    att

---
+++ obc-html5/TYPESCRIPT/enum/enum.ts
@@ -0,0 +1,8 @@
+enum Planets {
+  MERCURIO = 1,
+  VENUS,
+  TERRA,
+  MARTE,
+}
+
+Planets.TERRA;
---
+++ obc-html5/TYPESCRIPT/evitando-tipagem/exemplo-3.ts
@@ -0,0 +1,7 @@
+function verification(test) {
+  if (test) {
+  } else {
+    let _check: never;
+    return _check;
+  }
+}
