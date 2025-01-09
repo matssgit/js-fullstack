b422aaf12e281c7bc7d08273b76ca13db458a2dc
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2025-01-09 20:00:30 -0300

    new-project

---
+++ rocketseat/JAVASCRIPT/convert-template-main/scripts.js
@@ -0,0 +1,17 @@
+// Obtendo os elementos do formulário
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+
+// Manipulando o input amount para receber somente números.
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g;
+  amount.value = amount.value.replace(hasCharactersRegex, "");
+});
+
+// Capturando o evento de submit (enviar) do formulário.
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  console.log(currency.value);
+};
