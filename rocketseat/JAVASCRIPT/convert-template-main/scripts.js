94491d70ef0977fe9b834160e383e88d5aa3c0d9
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2025-01-10 16:26:00 -0300

    projeto

--- rocketseat/JAVASCRIPT/convert-template-main/scripts.js
+++ rocketseat/JAVASCRIPT/convert-template-main/scripts.js
@@ -1,7 +1,15 @@
+// Cotação de moedas do dia.
+const USD = 6.12;
+const EUR = 6.26;
+const GBP = 7.46;
+
 // Obtendo os elementos do formulário
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -13,5 +21,49 @@ amount.addEventListener("input", () => {
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  console.log(currency.value);
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+      break;
+    default:
+      alert("Opcão inválida!");
+  }
 };
+
+function convertCurrency(amount, price, symbol) {
+  try {
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+    // Calcula o resultado total.
+    let total = amount * price;
+
+    // Formatar o valor total.
+    total = formatCurrencyBRL(total).replace("R$", "");
+
+    // Exibe o resultado total.
+    result.textContent = `${total} Reais`;
+
+    // Aplica a classe que exibe o footer para mostrar o resultado.
+    footer.classList.add("show-result");
+  } catch (error) {
+    // Remove a classe do footer removendo ele da tela.
+    footer.classList.remove("show-result");
+
+    console.log(error);
+    alert("Não foi possível converter. Tente novamente mais tarde!");
+  }
+}
+
+// Formata a moeda em Real Brasileiro.
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+}
