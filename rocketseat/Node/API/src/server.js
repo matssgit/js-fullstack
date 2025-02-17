df66e98a883315b73d4d8464c7fae000050b35ed
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2025-02-17 14:34:58 -0300

    updates

---
+++ rocketseat/Node/API/src/server.js
@@ -0,0 +1,8 @@
+import http from "node:http";
+
+const server = http.createServer((request, response) => {
+  const { method } = request;
+  return response.writeHead(200).end("Método: " + method);
+});
+
+server.listen(3333);
