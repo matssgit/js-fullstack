9e2fc93da115afea2fb690a08d41fe2db26f66bb
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2025-02-23 21:36:12 -0300

    upd

---
+++ rocketseat/Node/suporte-tickets/src/database/database.js
@@ -0,0 +1,46 @@
+import fs from "node:fs/promises";
+import { json } from "node:stream/consumers";
+
+const DATABASE_PATH = new URL("db.json", import.meta.url);
+
+export class Database {
+  #database = {};
+
+  constructor() {
+    fs.readFile(DATABASE_PATH, "utf8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database));
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
+    }
+
+    this.#persist();
+  }
+
+  select(table, filters) {
+    let data = this.#database[table] ?? [];
+
+    if (filters) {
+      data = data.filter((row) => {
+        return Object.entries(filters).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
+    }
+
+    return data;
+  }
+}
--- rocketseat/Node/suporte-tickets/src/middlewares/routeHandler.js
+++ rocketseat/Node/suporte-tickets/src/middlewares/routeHandler.js
@@ -1,12 +1,21 @@
 import { routes } from "../routes/index.js";
+import { Database } from "../database/database.js";
+import { extractQueryParams } from "../utils/extractQueryParams.js";
+
+const database = new Database();
 
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    return route.method === request.method && route.path === request.url;
+    return route.method === request.method && route.path.test(request.url);
   });
 
   if (route) {
-    return route.controller({ request, response });
+    const routeParams = request.url.match(route.path);
+
+    const { query } = routeParams.groups;
+    request.query = query ? extractQueryParams(query) : {};
+
+    return route.controller({ request, response, database });
   }
 
   return response.writeHead(404).end("not found");
