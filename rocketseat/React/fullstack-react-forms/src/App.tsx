15d4286dafb9d89800e9e286855bee4f14a4d24e
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2025-03-08 23:14:53 -0300

    update

--- rocketseat/React/fullstack-react-forms/src/App.tsx
+++ rocketseat/React/fullstack-react-forms/src/App.tsx
@@ -22,7 +22,11 @@ const schema = yup.object({
 });
 
 export default function App() {
-  const { control, handleSubmit } = useForm<FormData>({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
     defaultValues: {
       name: "",
       date: "",
@@ -49,7 +53,9 @@ export default function App() {
           )}
         />
 
-        <span className="error">Nome é obrigatório</span>
+        {errors.name?.message && (
+          <span className="error">{errors.name.message}</span>
+        )}
 
         <Controller
           control={control}
@@ -58,6 +64,10 @@ export default function App() {
             <input type="date" placeholder="Nome do evento" {...field} />
           )}
         />
+
+        {errors.date?.message && (
+          <span className="error">{errors.date.message}</span>
+        )}
 
         <Controller
           control={control}
@@ -76,6 +86,10 @@ export default function App() {
           )}
         />
 
+        {errors.subject?.message && (
+          <span className="error">{errors.subject.message}</span>
+        )}
+
         <Controller
           control={control}
           name="description"
@@ -83,6 +97,10 @@ export default function App() {
             <textarea placeholder="Descrição" rows={4} {...field} />
           )}
         />
+
+        {errors.description?.message && (
+          <span className="error">{errors.description.message}</span>
+        )}
 
         <button type="submit">Salvar</button>
       </form>
