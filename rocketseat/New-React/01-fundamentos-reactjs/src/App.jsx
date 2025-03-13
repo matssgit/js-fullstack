c62a330e552a2ac0716bd6a88a5a53d389b4e0a8
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2025-03-13 18:56:05 -0300

    update

---
+++ rocketseat/New-React/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Matheus Santana"
+        content="Veniam nostrud ea do eu cupidatat cillum nulla occaecat dolor elit consectetur cillum."
+      />
+      <Post author="Diego Fernandes" content="Um post muito legal" />
+    </div>
+  );
+}
