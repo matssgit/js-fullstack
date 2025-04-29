2b5329a50fcd79d7736e3d920f12650df61fa4ce
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2025-04-28 21:15:01 -0300

    update

--- rocketseat/React/aulas/01-fundamentos-reactjs/src/App.jsx
+++ rocketseat/React/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import "./global.css";
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author="Matheus Santana"
         content="Eu laborum anim nulla esse incididunt commodo officia."
